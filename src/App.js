dbea413f9ee4e29e71c7e2651913248283834698
Heath C Brown <Heath.c.b@gmail.com>
2022-02-24 20:47:20 -0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import MovieInfo from './Components/movieInfo';
+function App() {
+  return (
+    <div className="App">
+      <h2>Rate Fast and Furious series</h2>
+        <MovieInfo/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/reviewList.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReviewForm from './reviewForm';
+//a container inside of a Movie that houses Review components
+const ReviewList = () => {
+    return (
+        <div>
+            <ReviewForm/>
+        </div>
+    )
+}
+
+export default ReviewList
